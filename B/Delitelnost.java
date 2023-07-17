673a6b57b6d7362289fb235db71bf281fdea905c
josse7cz <josefjanda@email.cz>
2023-07-17 09:11:43 +0200

    Delitelnost

---
+++ B/Delitelnost.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Delitelnost {
+    public static void main(String[] args) {
+    System.out.println("Program, který zjistí, zda jsou zadaná čísla dělitelná.");
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Zadejte první číslo: ");
+    int a = sc.nextInt();
+    System.out.println("Zadejte druhé číslo: ");
+    int b = sc.nextInt();
+    if(a%b==0){
+        System.out.println("Jsou dělitelná.");
+    }else{
+        System.out.println("Nedělitelná.");
+    }
+
+
+
+    }
+    
+}
