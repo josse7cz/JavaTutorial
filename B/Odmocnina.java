9ba7711db2f1bdbfda7093b018dd17b0d8673b33
josse7cz <josefjanda@email.cz>
2023-07-21 20:35:47 +0200

    Git stash sjednoceni

--- B/Odmocnina.java
+++ B/Odmocnina.java
@@ -5,27 +5,15 @@ public class Odmocnina {
         System.out.println("Program na odmocninu.");
         Scanner sc = new Scanner(System.in);
         System.out.println("Zadejte číslo pro odmocnění.");
-            
-        try {
-            int mocnenec = sc.nextInt();
-            if(mocnenec>0){
-               double result = Math.sqrt(mocnenec);
-               System.out.println("Výsledek: "+result); 
-            }else{
-                System.out.println("Záporné číslo nelze odmocnit");
-            }
-            
 
-
-        } catch (Exception e) {
-            System.out.println("Záporné číslo nelze odmocnit"+e);// TODO: handle exception
+        int mocnenec = sc.nextInt();
+        sc.close();
+        if (mocnenec > 0) {
+            double result = Math.sqrt(mocnenec);
+            System.out.println("Výsledek: " + result);
+        } else {
+            System.out.println("Záporné číslo nelze odmocnit");
         }
-        
-    
-        
-
-       
-    
     }
-    
+
 }
